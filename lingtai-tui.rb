56e16063ce94f7028e87f1da4cf7b6e0336b872d
huangzesen <hzsbazinga@outlook.com>
2026-04-04 20:30:17 -0700

    fix: update sha256 for version-stamped v0.4.30 binaries

--- lingtai-tui.rb
+++ lingtai-tui.rb
@@ -10,22 +10,22 @@ class LingtaiTui < Formula
   on_macos do
     on_arm do
       url "https://github.com/huangzesen/lingtai/releases/download/v0.4.30/lingtai-darwin-arm64.tar.gz"
-      sha256 "f4e33189a90f6a5b62d71a6b1cb32fc685ce946fa6242b6dd450a501204ff614"
+      sha256 "1986242ef5606336120a4df92f6df2156d3abb8f61a4183b2db392024b649a28"
     end
     on_intel do
       url "https://github.com/huangzesen/lingtai/releases/download/v0.4.30/lingtai-darwin-x64.tar.gz"
-      sha256 "97313b567a19c1e45adcc83058bfe77654c1783aa4f0994996cfee467ce4e4d6"
+      sha256 "7b3b9d9ccaaa52e18f4ff59e0f0c6e6f22420cfdca15aabc86244eccf08b056a"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/huangzesen/lingtai/releases/download/v0.4.30/lingtai-linux-arm64.tar.gz"
-      sha256 "0290d226f431ddd39e6e54a1e56ae322f4da1f19c7a1dc0bb14c9aa9ac9f9d0f"
+      sha256 "9623dee56792f4570f4c9f86b5c626505c3d99e685881598df98b8cbbdfde2c1"
     end
     on_intel do
       url "https://github.com/huangzesen/lingtai/releases/download/v0.4.30/lingtai-linux-x64.tar.gz"
-      sha256 "167d559b0fd2552e116ac320ed7840e2f4610c12a0b734c09cc60dce3b50c628"
+      sha256 "d5c5e24fa1262ae04dc4ca086ca1c288933f935442fb65572f77c9d36068f49b"
     end
   end
 
