416a4e9f001fac0cd97d905b5d0e6bba07e78c80
huangzesen <hzsbazinga@outlook.com>
2026-04-13 17:46:44 -0700

    fix: re-tar binaries with correct name (lingtai-tui)

--- lingtai-tui.rb
+++ lingtai-tui.rb
@@ -10,22 +10,22 @@ class LingtaiTui < Formula
   on_macos do
     on_arm do
       url "https://github.com/huangzesen/lingtai/releases/download/v0.4.42/lingtai-darwin-arm64.tar.gz"
-      sha256 "e6dc14b1351acfdc6503fde37b59d9f63135a7d4b11d1e67eac5172f593f8ec0"
+      sha256 "4de5daaf98b5f7316e0d14d508a017dacbf132e7c65f574ae78425c685e27e87"
     end
     on_intel do
       url "https://github.com/huangzesen/lingtai/releases/download/v0.4.42/lingtai-darwin-x64.tar.gz"
-      sha256 "8c426fa654073f0741a878f0634135661c78c204eb461803ff81fb4b25800291"
+      sha256 "f38856ed841c1acbc81eb525aa609632ef5442195629f571ca0ca44e43bca610"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/huangzesen/lingtai/releases/download/v0.4.42/lingtai-linux-arm64.tar.gz"
-      sha256 "59d224f9d2855dbd13ae060f78d54a3c3d9d3db64fb0f533fdffbe5dc9b8bea3"
+      sha256 "65e139e04977070b8cf3f52820602ed4d330caec4b5fa24cfd113b889e854d65"
     end
     on_intel do
       url "https://github.com/huangzesen/lingtai/releases/download/v0.4.42/lingtai-linux-x64.tar.gz"
-      sha256 "720ac7eac03c6f837213e0233ead1e2f98c0a9fc22aeb34c04bee54d38a47147"
+      sha256 "1c7f36d92db98be9f6cd4c087fd70233e8ab9ab01ded2a8942af970ef9430b08"
     end
   end
 
